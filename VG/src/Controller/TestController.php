732c5c38f83c30493138f6e3cd03f01406a63b52
frc <fcaumartin@gmail.com>
2023-11-15 16:19:11 +0100

    test controller

--- VG/src/Controller/TestController.php
+++ VG/src/Controller/TestController.php
@@ -2,15 +2,17 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Client;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TestController extends AbstractController
 {
-    #[Route('/test', name: 'app_test')]
-    public function index(): Response
+    #[Route('/test/{p}', name: 'app_test')]
+    public function index(Client $p): Response
     {
+        dd($p);
         return $this->render('test/index.html.twig', [
             'controller_name' => 'TestController',
         ]);
